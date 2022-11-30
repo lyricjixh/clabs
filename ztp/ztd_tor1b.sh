5e81a0c747dd6dd3aa6c69e7cb84f1c2e3e41086
Xianghui Ji <xianghui.ji@arista.com>
2022-11-30 09:31:31 -0500

    update the OOB network

--- ztp/ztd_tor1b.sh
+++ ztp/ztd_tor1b.sh
@@ -9,7 +9,7 @@
 ########## UPDATE THE BELOW CONFIG VARIABLES ACCORDINGLY ###########
 ########## ATLEAST ONE OF THEM SHOULD BE FILLED ####################
 #IMG_FILE=”http://50.0.0.1/OS10.bin”
-CLI_CONFIG_FILE="http:///172.17.60.134/cli_config_tor1b"
+CLI_CONFIG_FILE="http:///172.100.100.134/cli_config_tor1b"
 #POST_SCRIPT_FILE="http://50.0.0.1/no_post_script.py"
 ################### DO NOT MODIFY THE LINES BELOW #######################
 sudo os10_ztd_start.sh "$IMG_FILE" "$CLI_CONFIG_FILE" "$POST_SCRIPT_FILE"
